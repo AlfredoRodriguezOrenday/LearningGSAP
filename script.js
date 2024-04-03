bf62f9fbeafd821ad045d37692f4c6bb7e991b3f
AprendidendoGithub <R181308546@utna.edu.mx>
2024-04-02 21:12:27 -0600

    Adding more animations

--- script.js
+++ script.js
@@ -5,15 +5,30 @@ const tl = gsap.timeline({
 
 tl.from('.card_1',{
     duration: 2,
-    y:'-80vh'
+    y:'-80vh',
+    ease: 'power3.inOut'
 })
 
 tl.from('.card_2',{
     duration: 2,
-    x:'-55vh'
+    x:'-55vh',
+    ease: 'power3.inOut'
 }, '=-1.5')
 
 tl.from('.card_3',{
     duration: 2,
-    x:'60vh'
+    x:'60vh',
+    ease: 'power3.inOut'
+}, '=-2')
+
+tl.from('.card_4',{
+    duration: 2,
+    y:'30vh',
+    ease: 'power3.inOut'
+}, '=-2')
+
+tl.from('.card_5',{
+    duration: 2,
+    y:'30vh',
+    ease: 'power3.inOut'
 }, '=-2')
